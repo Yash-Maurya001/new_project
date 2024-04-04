7487b8ccdc54a67b698a11118f3db06423cf4b47
yash maurya <yashmaurya.in@gmail.com>
2024-04-04 21:44:26 +0530

    Add StickyScrollRevealDemo component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { StickyScrollRevealDemo } from "@/components/GettingStarted"
 import { HeroHighlightDemo } from "@/components/HeroMain"
 
 export default function Home() {
 	return (
 		<main className="w-full min-h-screen  bg-black">
 			<HeroHighlightDemo />
+			<StickyScrollRevealDemo />
 		</main>
 	)
 }
