1748467917ced8dad14a1b59c0e34a99e897d649
yash maurya <yashmaurya.in@gmail.com>
2024-04-04 21:56:51 +0530

    Remove StickyScrollRevealDemo component from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { StickyScrollRevealDemo } from "@/components/GettingStarted"
 import { HeroHighlightDemo } from "@/components/HeroMain"
 
 export default function Home() {
 	return (
 		<main className="w-full min-h-screen  bg-black">
 			<HeroHighlightDemo />
-			<StickyScrollRevealDemo />
 		</main>
 	)
 }
