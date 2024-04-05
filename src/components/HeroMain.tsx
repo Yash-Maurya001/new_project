b2be953991fc9183f1ff66d51701c03cbb13757f
yash maurya <yashmaurya.in@gmail.com>
2024-04-05 16:13:12 +0530

    Update styles in HeroMain and HeroHighlight components

--- src/components/HeroMain.tsx
+++ src/components/HeroMain.tsx
@@ -18,11 +18,11 @@ export function HeroHighlightDemo() {
 					duration: 0.5,
 					ease: [0.4, 0.0, 0.2, 1],
 				}}
-				className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto my-[5vh] "
+				className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold  text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto my-[5vh] "
 			>
 				With insomnia, nothing&apos;s real. Everything is far away. Everything
 				is a{" "}
-				<Highlight className="text-black dark:text-white">
+				<Highlight className="text-white">
 					copy, of a copy, of a copy.
 				</Highlight>
 			</motion.h1>
--- src/components/ui/hero-highlight.tsx
+++ src/components/ui/hero-highlight.tsx
@@ -36,9 +36,9 @@ export const HeroHighlight = ({
 			)}
 			onMouseMove={handleMouseMove}
 		>
-			<div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800  pointer-events-none" />
+			<div className="absolute inset-0 bg-dot-thick-neutral-800  pointer-events-none" />
 			<motion.div
-				className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
+				className="pointer-events-none  bg-dot-thick-indigo-500   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
 				style={{
 					WebkitMaskImage: useMotionTemplate`
             radial-gradient(
@@ -57,7 +57,7 @@ export const HeroHighlight = ({
 				}}
 			/>
 			{showGradient && (
-				<div className="absolute inset-0 h-full w-full  pointer-events-none bg-white dark:bg-black [mask-image:radial-gradient(ellipse_at_center,transparent,white)]" />
+				<div className="absolute inset-0 h-full w-full  pointer-events-none bg-black [mask-image:radial-gradient(ellipse_at_center,transparent,white)]" />
 			)}
 			<div className={cn("relative z-20", className)}>{children}</div>
 		</div>
@@ -90,7 +90,7 @@ export const Highlight = ({
 				display: "inline",
 			}}
 			className={cn(
-				`relative inline-block pb-1   px-1 rounded-lg bg-gradient-to-r from-indigo-300 to-purple-300 dark:from-indigo-500 dark:to-purple-500`,
+				`relative inline-block pb-1   px-1 rounded-lg bg-gradient-to-r  from-indigo-500 to-purple-500`,
 				className
 			)}
 		>
