e78da5603d0b58b003d99b0ee697f5552dbe9019
yash maurya <yashmaurya.in@gmail.com>
2024-04-05 12:30:59 +0530

    Add @tabler/icons-react package and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import { FeaturePod } from "@/components/FeaturedPod"
+import { GettingStarted } from "@/components/GettingStarted"
 import { HeroHighlightDemo } from "@/components/HeroMain"
 
 export default function Home() {
 	return (
-		<main className="w-full min-h-screen  bg-black">
-			<HeroHighlightDemo />
+		<main className="  bg-black">
+			<div className="w-full min-h-screen flex justify-center items-center">
+				<HeroHighlightDemo />
+			</div>
+			<div className="w-full min-h-screen flex justify-center items-center">
+				<GettingStarted />
+			</div>
+			<div className="w-full min-h-screen flex justify-center items-center">
+				<FeaturePod />
+			</div>
 		</main>
 	)
 }
